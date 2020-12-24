e0411c6ad9ab7c2e786f3941babff8aaf81b98cc
kprajiv1980 <wussupbuddy@yahoo.com>
2020-12-24 21:13:24 +0530

    test

---
+++ DevopsBasics-master/test/webapp/launchJenkins.java
@@ -0,0 +1,15 @@
+package webapp;
+
+public class launchJenkins {
+
+	
+	public static void printname() {
+		
+		String test= "avnish tomar";
+		
+		System.out.println("my Name is :"+ test);
+		// TODO Auto-generated method stub
+
+	}
+
+}
